7ffd1b6e51beeb7b1dab13a661547ff1480086fa
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:55:38 +0500

    Add files via upload

---
+++ Qno34.js
@@ -0,0 +1,17 @@
+"use strict";
+// 34. Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+// loop methode
+Object.defineProperty(exports, "__esModule", { value: true });
+let favorit_pizza = ["Malai boti", "zinger crunch", "crown crust"];
+for (let pizzas of favorit_pizza) {
+    console.log(`i like ${pizzas}`);
+}
+console.log("i really love pizza");
+// foeEach methode
+let pizza = ["Malai boti", "zinger crunch", "crown crust"];
+pizza.forEach(pizzas => {
+    console.log(`i like ${pizzas}`);
+});
+console.log("i really love pizza");
